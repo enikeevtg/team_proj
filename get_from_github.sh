df62f7637fbb2024d8ad63f2ef98e62eb9060324
Tagir Enikeev <enikeev.tg@gmail.com>
2023-05-31 13:35:37 +0300

    prefix_for_github.sh

--- get_from_github.sh
+++ get_from_github.sh
@@ -3,7 +3,7 @@
 CUR_BRANCH=$(git branch | grep "*" | sed 's/* '// )  # sed is Stream EDitor
 
 git checkout test
-git pull  #from github test branch
+git pull  # get latest commit from remote test branch
 git checkout $CUR_BRANCH
 git merge test
 git push origin $CUR_BRANCH
